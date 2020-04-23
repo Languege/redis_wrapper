6c42c6874a559ac0138778667136938954512a7e
刘高云 <liugy@soulgame.com>
2020-04-23 18:01:31 +0800

    暴露连接参数

--- connect.go
+++ connect.go
@@ -9,17 +9,34 @@ import (
  *@author LanguageY++2013
  *2019/2/20 5:33 PM
  **/
+const(
+	Default_MaxIdle = 10
+	Default_IdleTimeout = time.Duration(240) * time.Second
+	Default_MaxActive = 20
+)
 var(
 	wrapper	*RedisWrapper
 )
 
 
-func InitConnect(ip, port, password string, options... interface{}) {
+func InitConnect(ip, port, password string,maxIdle, maxActive int, idleTimeout time.Duration, options... interface{}) {
 	if wrapper != nil {
 		return
 	}
 
-	wrapper = NewRedisWrapper(ip, port, password, 100, time.Duration(240) * time.Second, 200)
+	if maxIdle == 0 {
+		maxIdle = Default_MaxIdle
+	}
+
+	if idleTimeout == 0 {
+		idleTimeout = Default_IdleTimeout
+	}
+
+	if maxActive == 0 {
+		maxActive = Default_MaxActive
+	}
+
+	wrapper = NewRedisWrapper(ip, port, password, maxIdle, idleTimeout, maxActive)
 	if len(options) > 0 {
 		prefix, ok := options[0].(string)
 		if ok {
